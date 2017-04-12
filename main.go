d235d488f7c818b5ae28ed48aeff12fd87127cbd
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2017-04-12 21:28:38 +0200

    Make clients work

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ import (
 
 // Message is a websocket message sent from a client
 type Message struct {
-	Color  string  `json:"color"`
-	PosX   int     `json:"posX"`
-	PosY   int     `json:"posY"`
-	SpeedX float64 `json:"speedX"`
-	SpeedY float64 `json:"speedY"`
+	Color     string  `json:"color"`
+	PosX      float64 `json:"posX"`
+	PosY      float64 `json:"posY"`
+	VelocityX float64 `json:"velocityX"`
+	VelocityY float64 `json:"velocityY"`
 }
 
 // clients are the connected clients
