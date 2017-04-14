d36aa5d471bbda460ccb861a333ffa929f568ced
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2017-04-14 07:44:18 +0200

    Improve JS readability

--- public/assets/scripts/helpers.js
+++ public/assets/scripts/helpers.js
@@ -1,20 +1,40 @@
-function abs(percentage) {
-    return Math.max(w, h) * (percentage/100);
+// Convert screen coordinates from relative to absolute
+function abs(rel) {
+    return Math.max(w, h) * (rel/100);
 }
 
-function absH(percentage) {
-    return h * (percentage/100);
+// Convert vertical screen coordinates from relative to absolute
+function absH(rel) {
+    return h * (rel/100);
 }
 
-function absW(percentage) {
-    return w * (percentage/100);
+// Convert horizontal screen coordinates from relative to absolute
+function absW(rel) {
+    return w * (rel/100);
 }
 
+// Convert screen coordinates from absolute to relative
+function rel(abs) {
+    return (abs / Math.max(w,h)) * 100;
+}
+
+// Convert vertical screen coordinates from absolute to relative
+function relH(abs) {
+    return (abs / h) * -100;
+}
+
+// Convert horizontal screen coordinates from absolute to relative
+function relW(abs) {
+    return (abs / w) * 100;
+}
+
+// Calculate the current screen size
 function calcScreenSize() {
     w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 }
 
+// Check if a ball and a target collide
 function doCollide(t, b) {
     var xMatch = false;
     var yMatch = false;
@@ -22,23 +42,26 @@ function doCollide(t, b) {
     if (b.posX+b.radius >= t.posX && b.posX-b.radius <= t.posX+t.width) {
         xMatch = true;
     }
-    if (b.posY+b.radius >= t.posY && b.posY-b.radius <= t.posY+t.height) {
+    if (b.posY+b.radius >= t.posY-t.height && b.posY-b.radius <= t.posY) {
         yMatch = true;
     }
 
     if (xMatch && yMatch) {
         return true;
-    } else {
-        return false
     }
+
+    return false
 }
 
+// Generate random integer
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+// Generate random hex color value
 function randomColor() {
-    var letters = '0123456789ABCDEF';
+    const letters = '0123456789ABCDEF';
+
     var color = '#';
 
     for (var i = 0; i < 6; i++) {
@@ -48,18 +71,7 @@ function randomColor() {
     return color;
 }
 
-function rel(val) {
-    return (val / Math.max(w,h)) * 100;
-}
-
-function relH(val) {
-    return (val / h) * -100;
-}
-
-function relW(val) {
-    return (val / w) * 100;
-}
-
+// Return correct WebSocket protocol
 function wsProtocol() {
     var p = 'ws';
 
