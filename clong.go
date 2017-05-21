d5d64dcd6b162b2378d47f039ad4f2b65e5689b0
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2017-05-21 17:51:50 +0200

    Add package comment

---
+++ clong.go
@@ -0,0 +1,3 @@
+// Package clong is a simple game that allows controller- and screen devices to connect to each other.
+// The goal of the game is to hit targets on the screen by flicking balls at them from the controller.
+package clong
