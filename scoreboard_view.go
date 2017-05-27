759dffe8d93d63ed21b4c821fa397966e47d2515
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2017-05-27 15:05:05 +0200

    Add scoreboard view

---
+++ scoreboard_view.go
@@ -0,0 +1,13 @@
+package clong
+
+import (
+	"net/http"
+	"path"
+)
+
+// ScoreboardViewHandler returns the screen HTML page.
+func ScoreboardViewHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, path.Join("public", "scoreboard.html"))
+	})
+}
