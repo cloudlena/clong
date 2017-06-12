6ae47349003c8e7214ead0facec835e0e26e8bb6
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2017-06-12 12:33:19 +0200

    Show URL on main screen

--- screen_view.go
+++ screen_view.go
@@ -1,7 +1,6 @@
 package clong
 
 import (
-	"fmt"
 	"net/http"
 	"path"
 )
@@ -9,7 +8,6 @@ import (
 // ScreenViewHandler returns the screen HTML page.
 func ScreenViewHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("TLS", r.TLS)
 		http.ServeFile(w, r, path.Join("public", "screen.html"))
 	})
 }
